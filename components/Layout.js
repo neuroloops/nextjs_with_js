c466baa622d79fa65761337db6b123be217bcf83
ludo loops <ludoloops@protonmail.com>
2021-06-09 02:52:48 +0200

    Feat: navbar

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Layout.module.css'
+import Navbar from './Navbar'
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+    </>
+  )
+}
+export default Layout
